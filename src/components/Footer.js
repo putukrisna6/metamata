2561c72e8599f176ccd7eb1c260e82503ca4b7d3
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-02-02 20:22:37 +0700

    main structure

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h3 className="footer-title">Developed By...</h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <nav className="navigation-clean">
+                <h1 className="header-title">RecipeBooks</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
